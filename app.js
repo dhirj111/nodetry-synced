ba8648e4ad0fe1e40952eb406fb1bbd4fdda3ced
dhirj111 <jdreamcreations@gmail.com>
2024-12-10 12:01:42 +0530

    [1]  created models cart for storing cart of diffrent user and cart-item of for storing cart item of user in cart , wrote and explained relations in app.js along with creating dummy cart  [2] implemeted get cart controller to fetch all carts* [3] implemented post cart controller to post add-to-cart clicked item to cart-item table with all fields

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const Sequelize = require('sequelize');
 const sequelize = require('./util/database');
-
 const app = express();
 const Product = require('./models/product');
 const User = require('./models/user')
+
+//imported cart and cart-item so that we can use diffrent associations here
+const Cart = require('./models/cart')
+const CartItem = require('./models/cart-item')
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
@@ -22,12 +25,11 @@ app.use((req, res, next) => {
   console.log("inside  recently build method")
   User.findByPk(1).then(user => {
     req.user = user;
-    console.log("suer is her e as" , user ,"ended here ___________454_____")
+    console.log("suer is her e as", user, "ended here ___________454_____")
     next();
     //important next() to make this middleware jump
   }).catch(err =>
     console.log(err)
-    
   )
 });
 app.use('/admin', adminRoutes);
@@ -37,10 +39,31 @@ Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
 //it(belong to) automatically adds new users id column to product table)
 
 User.hasMany(Product)
-//This means that one User can be associated with multiple Products
+//This means that one User can be associated with multiple Products or he 
+//can own multiple products as a seller
 
-//if we use sequlize.sync then it automatically uploads data to matching columns table
-//if table doesn't exist then it creates by using model files 
+//----------------------------------------
+
+User.hasOne(Cart)
+//A User can have one Cart. The Cart table will get a foreign key, typically 
+//named userId, to link the cart to its user.
+// resultant - A new column userId is added to store the ID of the user who owns the cart.
+
+Cart.belongsTo(User)
+//this synatx is optional and same as above  both adds a key to cart 
+//It means each Cart belongs to one User ,This is just inverse of above
+//The Cart table is linked to the User table through a foreign key, userId.
+
+Cart.belongsToMany(Product, { through: CartItem });
+//This establishes a many-to-many relationship between Cart and Product via an 
+//intermediate table/model called CartItem
+//A Cart can contain many Products.
+
+Product.belongsToMany(Cart, { through: CartItem });
+//This establishes the inverse many-to-many relationship, where a Product can belong to multiple Carts.
+//A Product can appear in multiple Carts.
+//The relationship is also defined through the same CartItem junction table.
+
 sequelize.sync().then(result => {
   return User.findByPk(1)
   //here it just sees if user already exists otherwise we jump to next .then to create a dummy user 
@@ -52,7 +75,14 @@ sequelize.sync().then(result => {
     }
     return user;
   })
+
   .then(user => {
+
+    return user.createCart()
+    //sequalize magic to create Cart with prevalue of userId in cart
+
+  })
+  .then(cart => {
     app.listen(4000)
   })
   .catch(err => {
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
 const Cart = require('../models/cart');
+const { where } = require('sequelize');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -52,32 +53,82 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.findAll(products => {
-      const cartProducts = [];
-      for (product of products) {
-        const cartProductData = cart.products.find(
-          prod => prod.id === product.id
-        );
-        if (cartProductData) {
-          cartProducts.push({ productData: product, qty: cartProductData.qty });
-        }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts
-      });
-    });
-  });
+
+  req.user.getCart()
+    .then(cart => {
+      console.log('cart is her e ===', cart)
+      return cart.getProducts().then(products => {
+        console.log('product  is her e ===', products)
+        res.render('shop/cart', {
+          path: '/cart',
+          pageTitle: 'Your Cart',
+          products: products
+        })
+      })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+
+
+  //old way
+  // Cart.getCart(cart => {
+  //   Product.findAll(products => {
+  //     const cartProducts = [];
+  //     for (product of products) {
+  //       const cartProductData = cart.products.find(
+  //         prod => prod.id === product.id
+  //       );
+  //       if (cartProductData) {
+  //         cartProducts.push({ productData: product, qty: cartProductData.qty });
+  //       }
+  //     }
+  //     res.render('shop/cart', {
+  //       path: '/cart',
+  //       pageTitle: 'Your Cart',
+  //       products: cartProducts
+  //     });
+  //   });
+  // });
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchedCart;
+  //declared it here so that we can use that cart in all then block once stored
+  //we got access to cart from here same way in get controller above
+  req.user.getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      console.log("index 102 fecthedCart" , fetchedCart)
+      //now fetched can be used in all blocks
+      return cart.getProducts({ where: { id: prodId } });
+      //this returns  clciked product row containg all product from products of mysql
+    }
+      //by above getProducts access to product where addtocart button is clicked
+      //in next then block we see thta whether product is already there or not if there 
+      //then quanitiy +1
+    )
+    .then(products => {
+      console.log("index 111 products" ,products)
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      let newQuantity = 1;
+      if (product) {
+        Cart.findByPk()
+        // if product is already there then we will increase newQuantity
+      }
+      return Product.findByPk(prodId).then(product => {
+        console.log("index 122 product to added" ,product)
+        return fetchedCart.addProduct(product, { through: { quantity: newQuantity } })
+        //.add product is magic syntax of sequelize  
+        //this will populate cart-item table with product id  and cartid and quantity
+      })
+    }).then(() => {
+      res.redirect('/cart')
+    })
+    .catch(err => console.log(err))
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
